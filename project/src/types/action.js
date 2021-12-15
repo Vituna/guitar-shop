d23d85fbdec72688bfaa76c1a5ef94421233852d
Viktor <vituna9@bk.ru>
2021-12-15 18:42:07 +0300

    Перенастраивает сборку

--- project/src/types/action.js
+++
@@ -1,3 +0,0 @@
-export const ActionType = {
-  LoadGuitars: 'data/loadGuitars',
-};
--- src/components/main/main.js
+++ src/components/main/main.js
@@ -1,3 +1,5 @@
+import Search from '../search/search'
+
 import  GuitarsList  from '../guitars-list/guitars-list';
 
 function Main() {
@@ -19,25 +21,10 @@ function Main() {
               </li>
             </ul>
           </nav>
-          <div className="form-search">
-            <form className="form-search__form">
-              <button className="form-search__submit" type="submit">
-                <svg className="form-search__icon" width="14" height="15" aria-hidden="true">
-                  <use href="#icon-search"></use>
-                </svg><span className="visually-hidden">Начать поиск</span>
-              </button>
-              <input className="form-search__input" id="search" type="text" autoComplete="off" placeholder="что вы ищите?" />
-              <label className="visually-hidden" htmlFor="search">Поиск</label>
-            </form>
-            <ul className="form-search__select-list hidden">
-              <li className="form-search__select-item" tabIndex="0">Четстер Plus</li>
-              <li className="form-search__select-item" tabIndex="0">Четстер UX</li>
-              <li className="form-search__select-item" tabIndex="0">Четстер UX2</li>
-              <li className="form-search__select-item" tabIndex="0">Четстер UX3</li>
-              <li className="form-search__select-item" tabIndex="0">Четстер UX4</li>
-              <li className="form-search__select-item" tabIndex="0">Четстер UX5</li>
-            </ul>
-          </div><a className="header__cart-link" href="!#" aria-label="Корзина">
+
+          <Search />
+
+          <a className="header__cart-link" href="!#" aria-label="Корзина">
             <svg className="header__cart-icon" width="14" height="14" aria-hidden="true">
               <use href="#icon-basket"></use>
             </svg><span className="visually-hidden">Перейти в корзину</span><span className="header__cart-count">2</span></a>
