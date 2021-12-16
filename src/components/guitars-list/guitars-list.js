9913b8c52d145c8fac20bd47fdfecefbef654c33
Viktor <vituna9@bk.ru>
2021-12-16 21:35:11 +0300

    Убирает перерисовку каталога по поиску

--- src/components/guitars-list/guitars-list.js
+++ src/components/guitars-list/guitars-list.js
@@ -1,21 +1,16 @@
 import { useSelector } from 'react-redux';
 
 import { getGuitars } from '../../store/guitar/selectors';
-import { getCurrentSearch } from '../../store/filters/selectors';
 
 import  GuitarOffer  from '../guitar-offer/guitar-offer';
 
-import { getFilterGuitarsName } from '../../utils';
-
 function GuitarsList() {
   const guitars = useSelector(getGuitars);
-  const currentSearch = useSelector(getCurrentSearch);
-
 
   return (
     <div className="cards catalog__cards">
 
-      {getFilterGuitarsName(guitars, currentSearch).map((guitar) => (
+      {guitars.map((guitar) => (
         <GuitarOffer key={guitar.id} guitar={guitar}/>
       ))}
 
